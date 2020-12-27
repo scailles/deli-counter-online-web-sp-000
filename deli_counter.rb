e2e639703f0fa0dc4916a8201c0d91e9c1ebacb1
Shaina C <scailles23@gmail.com>
2020-12-27 09:59:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,5 +21,13 @@ end
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
-  end
+end
   
+  
+def now_serving(katz_deli)
+  if katz_deli.size = 0
+    puts "The line is currently empty."
+    
+  else "Now serving #{katz_deli.[0].join}."
+end
+end
