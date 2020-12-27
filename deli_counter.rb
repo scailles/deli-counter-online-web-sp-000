dfbe6d6372a07483d7a1a3001eedd84ea0564335
Shaina C <scailles23@gmail.com>
2020-12-27 09:44:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     puts "The line is currently empty."
     
   else 
-    katz_deli.each do |name, index+=1|
+    katz_deli.each do |name, index|
       puts "#{index}. #{name} "
     end
   end
