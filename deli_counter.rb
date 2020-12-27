ab5813897d3af04793d5967dc6adecdcd73e65b8
Shaina C <scailles23@gmail.com>
2020-12-27 09:45:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,13 @@
 katz_deli = []
 
 def line(katz_deli)
+  line_list = []
   if katz_deli.size == 0
     puts "The line is currently empty."
     
   else 
     katz_deli.each do |name, index|
-      puts "#{index}. #{name} "
+      line_list << "#{index}. #{name} "
     end
   end
   
